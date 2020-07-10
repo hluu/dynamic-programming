be4d90ee792fb0574438983a6f1e363e26aed899
Hien Luu <hien.luu@doordash.com>
2020-07-09 19:09:13 -0700

    added logic for printing out which coins are used to make change

--- src/main/java/org/learning/dp/CoinChange.java
+++ src/main/java/org/learning/dp/CoinChange.java
@@ -2,7 +2,7 @@ package org.learning.dp;
 
 import org.testng.Assert;
 
-import java.util.Arrays;
+import java.util.*;
 
 
 /**
@@ -19,6 +19,7 @@ import java.util.Arrays;
  * Time complexity is O(nm) => n is amount, m is the # of denominations
  * Space complexity is O(n) => n is the amount
  *
+ *  https://leetcode.com/problems/coin-change/
  *
  * @author hluu
  *
@@ -29,11 +30,16 @@ public class CoinChange {
 
 
         test(6, new int[] {1,3,4}, 2);
+        test(4, new int[] {1,5}, 4);
         test(0, new int[] {1,3,4}, 0);
         test(10, new int[] {1,5,7}, 2);
         test(8, new int[] {1,5,7}, 2);
         test(11, new int[] {1,5,7}, 3);
         test(12, new int[] {1,5,7}, 2);
+        test(2, new int[] {5,7}, -1);
+        test(11, new int[] {5,7}, -1);
+        test(3, new int[] {2,4}, -1);
+        test(7, new int[] {2,4}, -1);
 
     }
 
@@ -47,24 +53,51 @@ public class CoinChange {
         Assert.assertEquals(actual, expected);
     }
 
-    public static int coinChange(int amount, int[] denominations) {
-        int minCoin[] = new int[amount+1];
+    public static int coinChange(int amount, int[] coins) {
+        int table[] = new int[amount+1];
+        int coin_tracker[] = new int[amount+1];
 
+        Arrays.fill(table, Integer.MAX_VALUE);
+
+        table[0] = 0; // base case
         for (int amt = 1; amt <= amount; amt++) {
             int minSoFar = Integer.MAX_VALUE;
-            for (int j = 0; j < denominations.length; j++) {
-                // make sure the denomination is less than or equal to amount
-                int denomination = denominations[j];
-                if (amt >= denomination) {
-                    //if (denomination <= amt) {
-                    minSoFar = Math.min(minCoin[amt - denominations[j]], minSoFar);
+            for (int coin : coins) {
+                if (amt >= coin) {
+                    if (table[amt - coin] < minSoFar) {
+                        minSoFar = table[amt - coin];
+                        coin_tracker[amt] = coin;
+                    }
+
                 }
             }
-            minCoin[amt] = minSoFar + 1;
-            System.out.println("amount: " + amt + " minCoin: " + Arrays.toString(minCoin));
+            table[amt] = (minSoFar < Integer.MAX_VALUE) ? minSoFar + 1 : Integer.MAX_VALUE;
+
+        }
+        System.out.println("table: " + Arrays.toString(table));
+
+        // which coins were used
+        System.out.println("coin_tracker: " + Arrays.toString(coin_tracker));
+        System.out.println("coins: " + extractCoins(coin_tracker));
+
+        return (table[amount] == Integer.MAX_VALUE) ? -1 : table[amount];
+
+    }
+
+    private static List<Integer> extractCoins(int[] coin_trackder) {
+        int amount = coin_trackder.length - 1;
+
+        if (coin_trackder[amount] == 0) {
+            return Collections.emptyList();
+        }
+        List<Integer> result = new ArrayList<>();
+        int prevIdx = amount;
+        while (coin_trackder[prevIdx] != 0) {
+            result.add(coin_trackder[prevIdx]);
+            prevIdx = prevIdx - coin_trackder[prevIdx];
         }
 
-        return minCoin[amount];
+        return  result;
     }
 
 }
