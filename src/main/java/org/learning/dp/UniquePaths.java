4862bf3688f3dfc207eb4a7cff51ec594495f2b5
Hien Luu <hien.luu@doordash.com>
2020-07-12 13:12:11 -0700

    added bottom up solution to unique paths problem:

--- src/main/java/org/learning/dp/UniquePaths.java
+++ src/main/java/org/learning/dp/UniquePaths.java
@@ -1,5 +1,8 @@
 package org.learning.dp;
 
+import org.learning.common.ArrayUtils;
+import org.testng.Assert;
+
 /**
  * A robot is located at the top-left corner of a m x n grid
  * (marked 'Start' in the diagram below).
@@ -31,6 +34,7 @@ package org.learning.dp;
  *   - what would be the answer for up(row-1, col) + up(row, col-1)
  *     - base case (row=0, col=0) => then 1
  *
+ * https://leetcode.com/problems/unique-paths/
  *  https://leetcode.com/problems/unique-paths/discuss/405983/Easy-understand-Java-Solutions-with-Explanations-(DP-Top-down-Bottom-up-Linear-Space)
  */
 public class UniquePaths {
@@ -39,6 +43,10 @@ public class UniquePaths {
 
         test(3, 2, 3);
         test(3, 7, 28);
+        test(1, 7, 1);
+        test(7, 1, 1);
+        test(1, 1, 1);
+
     }
 
     private static void test(int row, int col, int expected) {
@@ -47,8 +55,13 @@ public class UniquePaths {
 
         int actual = bruteForce(row-1, col-1);
 
-        System.out.printf("expected: %d, actual: %d\n",
-                expected, actual);
+        int actual2 = uniqPaths(row, col);
+
+        System.out.printf("expected: %d, actual: %d, actual2: %d\n",
+                expected, actual, actual2);
+
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual2);
     }
 
     private static int bruteForce(int row, int col) {
@@ -68,5 +81,28 @@ public class UniquePaths {
             return 1;
         }
         return numPaths(i - 1, j) + numPaths(i, j - 1);
+    }
+
+    private static int uniqPaths(int m, int n) {
+        int[][] table = new int[m][n];
+
+        // first column
+        for (int row = 0; row < m; row++) {
+            table[row][0] = 1;
+        }
+
+        // first row
+        for (int col = 0; col < n; col++) {
+            table[0][col] = 1;
+        }
+
+        for (int row = 1; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                table[row][col] = table[row-1][col] + table[row][col-1];
+            }
+        }
+
+        ArrayUtils.printMatrix(table);
+        return table[m-1][n-1];
     }
 }
